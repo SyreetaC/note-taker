fe96242e5f9132eae62af51d34ed8f8896b87ecc
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-17 20:26:59 +0100

    boilerplate for server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const htmlRoutes = require("./routes/htmlRoutes");
+const viewRoutes = require("./routes/viewRoutes"),
 const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = 3000;
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", htmlRoutes);
+app.use("/", viewRoutes);
 app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
