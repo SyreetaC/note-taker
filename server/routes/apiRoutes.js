715e2629136f6d93a18c15784b3a693d4796dffc
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-17 20:31:23 +0100

    added boilerplate for routes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,2 +1,12 @@
 //a route to return a single requested note when clicked (from db)
 //create route for reading existing notes from db
+
+const { Router } = require("express");
+
+const {} = require("");
+
+const router = Router();
+
+router.get("/");
+
+module.exports = router;
--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -1,2 +1,13 @@
 //create route for writing a single note
 //push new notes into an array as they are entered
+//post requests
+
+const { Router } = require("express");
+
+const {} = require("");
+
+const router = Router();
+
+router.post("/");
+
+module.exports = router;
