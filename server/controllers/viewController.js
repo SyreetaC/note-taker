531c837d7b15d96c24d34b8d0319564e46a10448
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-18 22:53:32 +0100

    returned file path for each function

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,16 +1,18 @@
 const path = require("path");
-const fs = require("fs");
 
 //renders home page
 const getIndexHtml = (req, res) => {
   filePath = path.join(__dirname, "../../public", "index.html");
   res.sendFile(filePath);
+  return filePath;
 };
+//try catch??
 
 //renders notes page
 const getNotesHtml = (req, res) => {
   filePath = path.join(__dirname, "../../public", "notes.html");
   res.sendFile(filePath);
+  return filePath;
 };
 
 module.exports = {
