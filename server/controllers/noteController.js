62b2adeb4896e0689a878e96031888135929a4c8
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-18 22:45:39 +0100

    added psuedo code to note controller

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -1,10 +1,17 @@
 //all functions for note in here- e.g. CRUD operations
 
+// const writeNote = (req, res) => {};
+
 const getAllNotes = (req, res) => {
   res.send("getAllNotes");
+  // const filePath = path.join(__dirname, "../db/db.json");
+  // const data = fs.readFileSync(filePath, "utf-8");
+  // return data;
 };
 const getOneNote = (req, res) => {
   res.send("getOneNote");
 };
+
+// const deleteNote = (req, res) => {};
 
 module.exports = { getAllNotes, getOneNote };
--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -1,21 +1,16 @@
 const path = require("path");
 const fs = require("fs");
 
+//renders home page
 const getIndexHtml = (req, res) => {
   filePath = path.join(__dirname, "../../public", "index.html");
   res.sendFile(filePath);
 };
 
+//renders notes page
 const getNotesHtml = (req, res) => {
   filePath = path.join(__dirname, "../../public", "notes.html");
   res.sendFile(filePath);
-  //res is the response to the browser
-  //send the notes.html file, using "sendFile"
-  //use path.join and __dirname + whatever the path is
-  // res.sendFile(filePath);
-  // const filePath = path.join(__dirname, "../db/db.json");
-  // const data = fs.readFileSync(filePath, "utf-8");
-  // return data;
 };
 
 module.exports = {
