d081ac6f3aa7a82466d8c3201e09fb0f177cf128
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-22 23:32:36 +0100

    view controller refactored

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -3,16 +3,12 @@ const path = require("path");
 //renders home page
 const getIndexHtml = (req, res) => {
   const filePath = path.join(__dirname, "../../public", "index.html");
-  console.log(filePath);
   res.sendFile(filePath);
 };
-//try catch??
-//refactor later
 
 //renders notes page
 const getNotesHtml = (req, res) => {
   const filePath = path.join(__dirname, "../../public", "notes.html");
-  console.log(filePath);
   res.sendFile(filePath);
 };
 
