319d365580634c7ab8bf4baab584e4c462a07812
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-05-22 23:31:38 +0100

    writeto file updated

--- server/db/writeToFile.js
+++ server/db/writeToFile.js
@@ -2,9 +2,10 @@ const path = require("path");
 const fs = require("fs");
 
 //function to write data to the database
-const writeToDb = () => {
+const writeToDb = (data) => {
   const filePath = path.join(__dirname, "../db/db.json");
   fs.writeFileSync(filePath, JSON.stringify(data));
+  return JSON.parse(data);
 };
 
 module.exports = writeToDb;
